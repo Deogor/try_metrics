11fc1eab230d363f396aa47317000ba0c669e8d0
vsurov <vsurov@phoenixit.ru>
2021-05-28 13:10:09 +0300

    новый файл:    metrics.py

---
+++ metrics.py
@@ -0,0 +1,23 @@
+import os
+hostname = os.uname()[1]
+print(hostname)
+
+import socket
+print(socket.gethostbyname(socket.gethostname()))
+print(socket.gethostbyname(socket.getfqdn()))
+
+import shutil
+total, used, free = shutil.disk_usage("/")
+print("Total: %d GiB" % (total // (2**30)))
+print("Used: %d GiB" % (used // (2**30)))
+print("Free: %d GiB" % (free // (2**30)))
+
+f = open('index.html', 'w')
+f.write('network info' + '\n')
+f.write(hostname + '\n')
+f.write(socket.gethostbyname(socket.gethostname()) + '\n')
+f.write(socket.gethostbyname(socket.getfqdn()) + '\n')
+f.write('Disk Info' + '\n')
+f.write("Total: %d GiB" % (total // (2**30)) + '\n')
+f.write("Used: %d GiB" % (used // (2**30)) + '\n')
+f.write("Free: %d GiB" % (free // (2**30)) + '\n')
